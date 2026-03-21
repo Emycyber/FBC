737f493b19854ad3acf361927c0a58be68a4bbb7
Emycyber <emycyber@gmail.com>
2026-03-21 04:50:35 -0700

    Update homepage

--- FBC/settings.py
+++ FBC/settings.py
@@ -15,12 +15,11 @@ import environ
 import os
 import dj_database_url
 
-env = environ.Env()
-environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
